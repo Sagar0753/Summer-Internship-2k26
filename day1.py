82538b68930d5f2a676324bef7993f99a0b3f8f2
Sagar0753 <sagarprajapati29090@gmail.com>
2026-07-06 14:14:49 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,13 @@
+a = int(input("Value 1 :"))
+b = int(input("Value 2 :"))
+c = a + b
+#Method 1
+print ("Value 1 is",a)
+#Method 2
+print ("Value 1 is",a,"\nValue 2 is",b)
+#Method 3
+print (f"Value 1 is {a} Value 2 is {b} Value 3 is {c}")
+#Method 4
+print ("Value 1 is {} Value 2 is {} Value 3 is {}".format(a,b,c))
+
+
