ba07a00865fee20a1dd57362ada68c2a05e734a9
Sagar0753 <sagarprajapati29090@gmail.com>
2026-07-07 16:06:27 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,23 @@
+#Taking 5 numbers as input from user and calculating the maximum and sorting it in a ascending order without list
+
+Numbers = ['','','','','']
+sorted = ['','','','','']
+
+for i in range(len(Numbers)):
+    print("Enter number ",i,":",)
+    x = int(input())
+    Numbers[i] = x
+
+print(Numbers)
+
+Max = 0
+
+for i in range(0,5):
+    if Numbers[i] <= Max:
+        Max = Numbers[i]
+
+for i in range(0,5):
+    if Max == Numbers[i]:
+        Numbers[i] = 0
+        
+print("Maximum Number:", Max)
