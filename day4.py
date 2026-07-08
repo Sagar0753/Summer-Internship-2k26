375ce48a8e234f2ec0ab50078d09c8523ba6ff7a
Sagar0753 <sagarprajapati29090@gmail.com>
2026-07-08 14:28:36 +0530

    day4_V2.py
    Calculate area of a square.(Formula :- L*L)

--- day4.py
+++ day4.py
@@ -9,3 +9,11 @@ if Length == Breadth:
     print("The the Area of Sqaure is:",Area)
 else:
     print("The the Area of rectangle is:",Area)
+
+#Calculate area of a rectangle.
+
+Length = float(input("Enter the length of rectangle:"))
+Breadth = float (input("Enter the breadth of rectangle:"))
+Area = Length * Breadth
+
+print("The the Area of rectangle is:",Area)
