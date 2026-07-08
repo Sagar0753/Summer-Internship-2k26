fc43aa85f5568373fbaa743f497d31117de9ea9a
Sagar0753 <sagarprajapati29090@gmail.com>
2026-07-08 14:35:23 +0530

    day4_V3.py
    Added calculation for the area of a circle and its diameter.

--- day4.py
+++ day4.py
@@ -17,3 +17,12 @@ Breadth = float (input("Enter the breadth of rectangle:"))
 Area = Length * Breadth
 
 print("The the Area of rectangle is:",Area)
+
+#Calculate area of a circle.(Formula :- pi*r*r)
+
+pi = 3.14
+r = float(input("Enter the radius of Circle: "))
+d = 2 * r
+area = pi * r * r
+print("Are of Circle is:",area)
+print("Are of Diameter is:",d)
