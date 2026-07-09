d0be88f6f3970147ea8752a3783c947a45df825d
Sagar0753 <sagarprajapati29090@gmail.com>
2026-07-09 14:00:43 +0530

    day5_V2.py
    even odd que

--- day5.py
+++ day5.py
@@ -2,3 +2,10 @@
 F = float(input("Enter the Temperature in Fahrenheit:"))
 C = ((F-32)*5)/9 
 print("Temperature in Celsius is:",C)
+
+#Check whether number is even or odd.
+num = int(input("Enter any Number:"))
+if num % 2 == 0:
+    print("Entered Number is even")
+else:
+    print("Entered Number is odd")
