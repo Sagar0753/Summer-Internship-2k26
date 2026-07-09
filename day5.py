ca7f4fcffd1e4f1fa314b0935be838a865bd0c36
Sagar0753 <sagarprajapati29090@gmail.com>
2026-07-09 13:59:59 +0530

    day4_V1.py

---
+++ day5.py
@@ -0,0 +1,4 @@
+#Write a program to enter the temperature in Fahrenheit and convert it to Celsius.
+F = float(input("Enter the Temperature in Fahrenheit:"))
+C = ((F-32)*5)/9 
+print("Temperature in Celsius is:",C)
